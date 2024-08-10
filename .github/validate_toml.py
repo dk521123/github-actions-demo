81b30987ce5dbdb15bdbaee5c6e1af08ce198fcf
D <52901043+dk521123@users.noreply.github.com>
2024-08-10 11:50:12 +0900

    Create validate_toml.py

---
+++ .github/validate_toml.py
@@ -0,0 +1,9 @@
+import tomllib
+import sys
+
+# e.g. python3 validate_toml.py test.toml
+input_toml = sys.argv[1]
+
+with open(input_toml,'r') as file:
+  output = tomllib.load(file)
+  print(output)
